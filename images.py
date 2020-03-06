5d0797f47e8de48d0b21c551b8f44a78f15326e7
Aaron Glave <glaveaf@rose-hulman.edu>
2020-03-06 15:17:46 -0500

    refactor. make scryfall construce OnlineTTSCards

---
+++ images.py
@@ -0,0 +1,11 @@
+def openImage(filename):
+    #TODO get image data
+    pass
+
+def combine(rows, columns, imageData):
+    #TODO use pillow to build card sheet
+    pass
+
+def download(url, filename):
+    #TODO download image to file
+    pass
