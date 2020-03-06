5dbb4e19d401cd156ff857360fe6f967ba60ef2b
Aaron Glave <glaveaf@rose-hulman.edu>
2020-03-06 16:31:33 -0500

    add unit tests

--- cards.py
+++ cards.py
@@ -1,5 +1,4 @@
 import images
-from scryfall import ScryfallCardFactory
 from abc import ABC, abstractmethod
 import os
 import os.path
--- tests.py
+++ tests.py
@@ -1,11 +1,24 @@
 from scryfall import ScryfallCardFactory
+import cards
+import images
+import unittest
+
+class ScryfallTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.ds = ScryfallCardFactory(cards.cache_location('CardBack.jpg'))
+    def test_werewolf(self):
+        werewolf = self.ds.make_card('Gatstaf Arsonists // Gatstaf Ravagers')
+        self.assertEqual(werewolf.name, 'Gatstaf Arsonists')
+        print(werewolf.image)
+        print(werewolf._filename())
+
+    def test_forest(self):
+        forest = self.ds.make_card('Forest', 'C16')
+        self.assertEqual(forest.name, 'Forest')
+        self.assertEqual(forest.set, 'C16')
+        print(forest.image)
+        print(forest._filename())
+
 if __name__ == '__main__':
-    ds = ScryfallCardFactory(cards.cache_location('CardBack.jpg'))
-    forest = ds.make_card('Forest', 'C16')
-    werewolf = ds.make_card('Gatstaf Arsonists // Gatstaf Ravagers')
-    print(forest.name)
-    print(forest.image)
-    print(forest._filename())
-    print(werewolf.name)
-    print(werewolf.image)
-    print(werewolf._filename())
+    unittest.main()
